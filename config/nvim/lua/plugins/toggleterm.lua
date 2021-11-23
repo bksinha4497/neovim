9e8a2b3bf9061be9d38a707fadea79a1a7403b7a
Biswajit Kumar <bksinha4497@gmail.com>
2021-11-23 23:53:33 +0530

    updated ToggleTerm config

--- config/nvim/lua/plugins/toggleterm.lua
+++ config/nvim/lua/plugins/toggleterm.lua
@@ -1,4 +1,5 @@
 require("toggleterm").setup{
+  -- size can be a number or function which is passed the current terminal
   function(term)
     if term.direction == "horizontal" then
       return 15
@@ -14,7 +15,7 @@ require("toggleterm").setup{
   start_in_insert = true,
   insert_mappings = true, -- whether or not the open mapping applies in insert mode
   persist_size = true,
-  direction ='horizontal',
+  direction =  'horizontal', -- 'vertical' | 'horizontal' | 'window' | 'float',
   close_on_exit = true, -- close the terminal window when the process exits
   shell = vim.o.shell, -- change the default shell
   -- This field is only relevant if direction is set to 'float'
@@ -23,9 +24,9 @@ require("toggleterm").setup{
     -- see :h nvim_open_win for details on borders however
     -- the 'curved' border is a custom border type
     -- not natively supported but implemented in this plugin.
-    border = 'single',
-    width = 60,
-    height = 60,
+    border =  'single', -- 'single' | 'double' | 'shadow' | 'curved' | ... other options supported by win open
+    width = 90,
+    height = 400,
     winblend = 3,
     highlights = {
       border = "Normal",
